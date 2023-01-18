b5c0b1f2b1d690d00b80c5dc734f48d9314b464f
Alex Schokking <aschokking@gmail.com>
2023-01-17 19:45:11 -0800

    Update deploy.cmd so it deletes any existing python files first

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,6 @@
+REM delete any existing python files so we know it's only going to run our new ones
+del d:\*.py
+REM copy over our python files
 xcopy *.py d:\ /Y
+REM copy over our lib files
 xcopy lib d:\lib /Y
