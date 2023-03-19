702a23f99bc56260683bb3ad58b3ba23aae6832f
Alex Schokking <aschokking@gmail.com>
2023-03-18 17:48:05 -0700

    alliance pin reading and working, start work on rest of states

--- main.py
+++ main.py
@@ -1,142 +1,314 @@
 # SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
+from digitalio import DigitalInOut, Direction, Pull
 
 """CircuitPython Essentials NeoPixel example"""
 import time
 import board
 from rainbowio import colorwheel
 import neopixel
-#import random
 
-###############################################
-#   Setup variables
-###############################################
+pixel_pin = board.D5
+num_pixels = 20
 
-pixel_pin = board.D5    #pin the led strip is contected to
-num_pixels = 20         #number of light pixels on the light strip
-brightness = .3         #default brightness of strip
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness = .3, auto_write=False) #initialize the lightstip objectusing the default parameters
+PIN_ALLIANCE = 7
 
-alliance_length = 4
-alliance_color = (0,255,0) #default value is green so we can see if there a problem
-
-loop_counter = 0
-
-###############################################
-#   End setup variables
-###############################################
+print('Starting!')
 
 
-# test area
-i=0
-
-
-# end test area
-
-#sequence patterns
-def set_All(r, g, b):
-   pixels.brightness = brightness
-   for a in range(20):
-        if a < alliance_length:
-            pixels[a]=alliance_color
-            print('Alliance color:',alliance_color)
-        else:
-            pixels[a]=(r, g, b)
-   pixels.show()
-
-
-def set_TwoColors(r, g, b,r2,g2,b2):
-   pixels.brightness = brightness
-   for a in range(20):
-        if a < alliance_length:
-            pixels[a] = alliance_color
-        elif a%2 == 0:
-            pixels[a]=(r,g,b)
-        else: 
-            pixels[a]=(r2,g2,b2)
-   pixels.show()
-
-
-def set_ThreeColors(r,g,b,r2,g2,b2,r3,g3,b3): #this is not correctly assigning colors at the endpoints
-    pixels.brightness = brightness
-    for a in range(20):
-        if a < alliance_length:
-            pixels[a] = alliance_color
-        elif a % 3 == 0:
-            pixels[a] = (r,g,b)
-        elif a % 3 == 1:
-            pixels[a] = (r2,g2,b2)
-        else:
-            pixels[a] = (r3,g3,b3)
-    pixels.show()
-    time.sleep(.15)
-    
-#animations
-def animate_fade():
-    steps = 100
-    for a in range(steps):
-        pixels.brightness = a / steps
-        time.sleep(1/steps)
+###### knight Rider ######
+'''while True:
+    for forwards in range(20):
+        pixels[forwards]=(255, 0, 0)
+        pixels[forwards-1]=(75, 0, 0)
+        pixels[forwards-2]=(0, 0, 0)
+        #pixels[forwards+1]=(75, 0, 0)
+        #pixels[forwards+2]=(0, 0, 0)
+        if forwards == 18:
+            pixels[forwards]=(255, 0, 0)
+        if forwards == 19:
+            pixels[forwards]=(75, 0, 0)
         pixels.show()
-    
-    for a in range(steps,-1,-1):
-        pixels.brightness = a / steps
-        time.sleep(1/steps)
-
+        time.sleep(1)
+    for backwards in range(19, 0, -1):
+        pixels[backwards]=(0, 0, 0)
+        pixels[backwards-1]=(255, 0, 0)
         pixels.show()
+        time.sleep(0.09)'''
 
-def animate_colorchase(): 
-    temp_list = [(0,0,0)]*(20-alliance_length)
-    for a in range(len(temp_list)):
-        pos = (a + 1) % (20-alliance_length)
-        temp_list[pos] = pixels[a+alliance_length]
-    for a in range(len(temp_list)):
-        pixels[a+alliance_length] = temp_list[a]
+##### Rainbow Circle ######
+'''for rainbow in range(20):
+    pixels[rainbow]=colorwheel(255/20*rainbow)
     pixels.show()
-    time.sleep(.15)
 
-def animate_colorchase_slicer():
-    pixels.brightness = brightness
-    pixels[:] = pixels[:alliance_length]+pixels[alliance_length+1:]+pixels[alliance_length:alliance_length+1]
-    pixels.show()
-    time.sleep(.15)
-
-#unique states:
-def set_Alliance():
-    #read in from pin
-    global alliance_color
-    if 1 == 1:
-        alliance_color = (255,0,0)
-    else:
-        alliance_color = (0,0,255)
-
-
-#rand = random.randrange(8)
-#print('test')
+movingRainbow = 0
 while True:
-    print('Main loop num:',loop_counter)
+    for r in range(20):
+        pixels[r]=colorwheel((255/20*(r+movingRainbow))%255)  
+        #pixels[r]=colorwheel((r+movingRainbow)/20)
+        pixels.show()
+    movingRainbow = movingRainbow+1'''
 
 
-    #read alliance pin and set alliance color
-    set_Alliance()
-    set_All(0,255,0)
-    time.sleep(2)
+##### FUNCTIONS FOR ROBOT #######
 
+#pin7
+pin7 = DigitalInOut(board.D7)
+pin7.direction = Direction.INPUT
+pin7.pull = Pull.DOWN
 
+#pin9
+pin9 = DigitalInOut(board.D9)
+pin9.direction = Direction.INPUT
+pin9.pull = Pull.DOWN
+
+#pin10
+pin10 = DigitalInOut(board.D10)
+pin10.direction = Direction.INPUT
+pin10.pull = Pull.DOWN
+
+#pin11
+pin11 = DigitalInOut(board.D11)
+pin11.direction = Direction.INPUT
+pin11.pull = Pull.DOWN
+
+#pin12
+pin12 = DigitalInOut(board.D12)
+pin12.direction = Direction.INPUT
+pin12.pull = Pull.DOWN
+
+#pin13
+pin13 = DigitalInOut(board.D13)
+pin13.direction = Direction.INPUT
+pin13.pull = Pull.DOWN
+
+pin_alliance = pin7
+
+#allianceColor=(255, 0, 0)
+allianceColor=(0, 0, 255)
+
+def blinkingCube():
+    for cube in range(15, 20, 1):
+        pixels[cube]=(255, 0, 0)
+    for cube in range(0, 15):
+        pixels[cube]=(145, 0, 255)
+    pixels.show()
+    time.sleep(0.3)
+    for cube in range(15):
+        pixels[cube]=(0, 0, 0)
+    pixels.show()
+    time.sleep(0.2)
     
-    set_TwoColors(255,0,0,0,0,255)
-    time.sleep(2)
-    
-    set_ThreeColors(255,255,0,0,255,255,255,0,255)
-    time.sleep(2)
-    while True:
-        animate_colorchase()
-        #animate_colorchase_slicer()
-        animate_fade()
+#while True:
+   # blinkingCube()
 
 
-    #chase needs to keep running in order to work
 
-    loop_counter += 1
+def blinkingCone():
+    for cube in range(15, 20, 1):
+        pixels[cube]=(255, 0, 0)
+    for cone in range(0, 15):
+        pixels[cone]=(255, 165, 0)
+    pixels.show()
+    time.sleep(0.3)
+    for cone in range(15):
+        pixels[cone]=(0, 0, 0)
+    pixels.show()
+    time.sleep(0.2)
+
+#while True:
+    #blinkingCone()
+
+
+def enable():
+    def setAll(r, g, b):
+        for a in range(20):
+            pixels[a]=(r, g, b)
+        pixels.show()
+
+        for enable in range(20):
+            pixels[enable]=(255, 0, 0)
+
+        for t in range(256):
+            setAll(0, t, 0)
+        for t in range(255, -1, -1):
+            setAll(0, t, 0)
+#while True:
+    #enable()
+
+
+count=0 
+def enabled():
+    #while True:
+
+        #print('start of loop 1')
+        for alliance in range(15, 20, 1):
+            pixels[alliance]=allianceColor
+
+        steps=30
+        wait=0.04
+        for countdown in range(steps, 1, -1):
+            for enabled in range(0, 15):
+                pixels[enabled]=(0, 255*countdown/steps, 0)
+            pixels.show()
+            time.sleep(wait)
+            #print('brightness down')
+
+        for countup in range(1, steps, 1):
+            #print('in loop 1')
+            for enabled in range(0, 15):
+                pixels[enabled]=(0, 255*countup/steps, 0)
+            pixels.show()
+            time.sleep(wait)
+            #print('brightness up')
+
+#while True:
+ #   enabled()
+
+
+def no_code():
+    #while True:
+    for noCode in range(20):
+        pixels[noCode]=(255, 0, 0)
+    pixels.show()
+    time.sleep(0.3)
+    for noCode in range(20):
+        pixels[noCode]=(0, 0, 0)
+    pixels.show()
+    time.sleep(0.3)
+#while True:
+    #noCode()
+
+def disabled():
+    for disabled in range(0, 15):
+        pixels[disabled]=(255, 0, 0)
+        pixels.show()
+    for alliance in range(15, 20):
+        pixels[alliance]=(allianceColor)
+        pixels.show
+#while True:
+    #disabled()
+
+#new
+"""
+temp = pin13.value
+temp2 = pin12.value<<1
+temp3 = pin11.value<<2
+temp4 = pin10.value<<3
+temp5 = pin9.value<<4
+
+temp_total = (temp+temp2+temp3+temp4+temp5)
+print(temp_total)
+
+#pin7value
+print(pin7.value)
+
+#pin9value
+print(pin9.value)
+
+#pin10value
+print(pin10.value)
+
+#pin11value
+print(pin11.value)
+
+#pin12value
+print(pin12.value)
+
+#pin13value
+print(pin13.value)
+
+time.sleep(5)
+"""
+
+
+while True:
+
+    # Read from pins
+
+    if pin_alliance.value:
+        allianceColor = (0, 0, 255)
+    else:
+        allianceColor = (255, 0, 0)
+
+    temp = pin13.value
+    temp2 = pin12.value<<1
+    temp3 = pin11.value<<2
+    temp4 = pin10.value<<3
+    temp5 = pin9.value<<4
+
+    temp_total = (temp+temp2+temp3+temp4+temp5)
+    print('temp_total: ', temp_total)
+
+    if temp_total == 0:
+        no_code()
+    else:
+        enabled()
+
+
+    time.sleep(0.1)
+
+    # print('starting blinkingCube')
+    # blinkingCube()
+    # time.sleep(1)
+    # print('ending blinkingCube')
+
+    '''print('starting blinkingCone')
+    blinkingCone()
+    time.sleep(1)
+    print('ending blinkingCone')
+
+    print('starting enabled')
+    enabled()
+    time.sleep(1)
+    print('ending enabled')
+
+    print('starting noCode')
+    noCode()
+    time.sleep(1)
+    print('ending noCode')
+
+    print('starting disabled')
+    disabled()
+    time.sleep(1)
+    print('ending disabled')'''
+
+
+
+####### EXTRAS ########
+
+'''def lowBattery():
+    for lowBattery in range(20):
+        pixels[lowBattery]=(0, 255, 0)
+    pixels.show()
+    time.sleep(5/count)
+    for lowBattery in range(20):
+        pixels[lowBattery]=(240, 70, 0)
+    pixels.show()
+    time.sleep(1)
+
+    if count < 4:
+        for lowBattery in range(20):
+            pixels[lowBattery]=(0, 255, 0)
+        pixels.show()
+        time.sleep(5/count)
+        for lowBattery in range(20):
+            pixels[lowBattery]=(240, 70, 0)
+        pixels.show()
+        time.sleep(1)
+
+    elif count >= 4 and count >= 8:
+        for lowBattery in range(20):
+            pixels[lowBattery]=(240, 70, 0)
+        pixels.show()
+        time.sleep(5/(count-3))
+        for lowBattery in range(20):
+            pixels[lowBattery]=(255, 0, 0)
+        pixels.show()
+        time.sleep(1)
+
+    else:
+        pixels[lowBattery]=(255, 0, 0)'''
